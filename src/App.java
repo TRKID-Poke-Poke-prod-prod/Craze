217ddcfd8fa63a39508e7666e5c3944463f762d4
TRKID-Poke-Poke-prod-prod <luckytilakrao@gmail.com>
2025-01-28 12:09:46 +1100

    Verification system

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Lil nigga");
+        /*
+        if (args.length==0){
+            System.out.println("Please use a verified verion of our product");
+            System.exit(0);
+        }
+        System.out.println("Welcome to CRAZE!");
+        String playerId = args[3];
+        int authCode = Integer.parseInt(args[0]);
+        int differencial = Integer.parseInt(args[1]);
+        int checkCode = Integer.parseInt(args[2]);
+        int code = authCode*(differencial-(authCode * differencial)/3)+19*authCode;
+        if (code!=checkCode){
+            System.out.println(checkCode);
+            System.out.println("Please use a verified verion of our product");
+            System.exit(0);
+        }
+        */
     }
 }
